418214a9d5133e5f950c72d3c5ccdd7ff4ac87a8
rohanvengurlekar <rohanvengurlekar1997@gmail.com>
2022-04-07 12:28:05 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,7 @@
+function display()
+{
+var x=document.details.fname.value;
+var y=document.details.email.value;
+var z=document.details.phnum.value;
+alert("FIRST NAME:"+x+"     "+"Email:"+y+"     "+"PHONE NUMBER:"+z);
+}
